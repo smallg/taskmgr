ed3ba3da41b56a47743b4012ebcdf9fb1af8967c
gwpxjb@163.com <gwpxjb@163.com>
2018-03-21 14:29:35 +0800

    2-3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-
-import { AppComponent } from './app.component';
-
+import {AppComponent} from './app.component';
+import {CoreModule} from './core/core.module';
+import {MdSidenavModule} from '@angular/material';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        CoreModule,
+        MdSidenavModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,30 @@
+import {NgModule, Optional, SkipSelf} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MdToolbarModule} from '@angular/material';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        MdToolbarModule
+    ],
+    declarations: [
+        HeaderComponent,
+        FooterComponent,
+        SidebarComponent
+    ],
+    exports: [
+        HeaderComponent,
+        FooterComponent,
+        SidebarComponent
+    ]
+})
+export class CoreModule {
+    constructor(@Optional() @SkipSelf() parent: CoreModule) {
+        if (parent) {
+            throw new Error('core module existed!');
+        }
+    }
+}
