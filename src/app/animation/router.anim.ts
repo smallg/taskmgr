77d1687122532000d6360f9c439f44406db0b10e
gwpxjb@163.com <gwpxjb@163.com>
2018-03-23 23:00:34 +0800

    3-4 add animation for new card

--- src/app/animation/router.anim.ts
+++ src/app/animation/router.anim.ts
@@ -20,8 +20,8 @@ export const slideToRight = trigger('routeAnim', [
 ]);
 
 const slideToBottom = trigger('routeAnim', [
-    state('void', style({position: 'fixed', width: '100%', height: '80%'}) ),
-    state('*', style({position: 'fixed', width: '100%', height: '80%'}) ),
+    state('void', style({position: 'fixed', width: '100%', height: '80%'})),
+    state('*', style({position: 'fixed', width: '100%', height: '80%'})),
     transition(':enter', [
         style({transform: 'translateY(-100%)'}),
         animate('0.5s ease-in-out', style({transform: 'translateY(0%)'}))
@@ -33,8 +33,8 @@ const slideToBottom = trigger('routeAnim', [
 ]);
 
 const slideToTop = trigger('routeAnim', [
-    state('void', style({position: 'fixed', width: '100%', height: '100%'}) ),
-    state('*', style({position: 'fixed', width: '100%', height: '100%'}) ),
+    state('void', style({position: 'fixed', width: '100%', height: '100%'})),
+    state('*', style({position: 'fixed', width: '100%', height: '100%'})),
     transition(':enter', [
         style({transform: 'translateY(100%)'}),
         animate('0.5s ease-in-out', style({transform: 'translateY(0%)'}))
--- src/app/project/project-list/project-list.component.ts
+++ src/app/project/project-list/project-list.component.ts
@@ -4,19 +4,21 @@ import {NewProjectComponent} from '../new-project/new-project.component';
 import {InviteComponent} from '../invite/invite.component';
 import {ConfirmDialogComponent} from '../../shared/confirm-dialog/confirm-dialog.component';
 import {slideToRight} from '../../animation/router.anim';
+import {listAnimation} from '../../animation/list.anim';
 
 @Component({
     selector: 'app-project-list',
     templateUrl: './project-list.component.html',
     styleUrls: ['./project-list.component.scss'],
     animations: [
-        slideToRight
+        slideToRight,
+        listAnimation
     ]
 })
 export class ProjectListComponent implements OnInit {
     projects = [
-        {'name': '企业写作平台', 'desc': '这是一个企业内部项目', 'coverImg': '/assets/img/covers/0.jpg'},
-        {'name': '测试平台', 'desc': '这是一个测试平台', 'coverImg': '/assets/img/covers/1.jpg'}
+        {'id': 1, 'name': '企业写作平台', 'desc': '这是一个企业内部项目', 'coverImg': '/assets/img/covers/0.jpg'},
+        {'id': 2, 'name': '测试平台', 'desc': '这是一个测试平台', 'coverImg': '/assets/img/covers/1.jpg'}
     ];
 
     @HostBinding('@routeAnim') state;
@@ -29,7 +31,11 @@ export class ProjectListComponent implements OnInit {
 
     openNewProjectDialog() {
         const dialogRef = this.dialog.open(NewProjectComponent, {data: {title: '新建项目'}});
-        dialogRef.afterClosed().subscribe(result => console.log(result));
+        dialogRef.afterClosed().subscribe(result => {
+            console.log(result);
+            this.projects = [...this.projects, {'id': 3, 'name': '新项目', 'desc': '这是一个新项目', 'coverImg': '/assets/img/covers/0.jpg'},
+                {'id': 4, 'name': '又一个新项目', 'desc': '这是又一个新项目', 'coverImg': '/assets/img/covers/0.jpg'}];
+        });
     }
 
     launchUpdateDialog() {
@@ -37,9 +43,12 @@ export class ProjectListComponent implements OnInit {
         dialogRef.afterClosed().subscribe(result => console.log(result));
     }
 
-    launchDeleteDialog() {
+    launchDeleteDialog(project) {
         const dialogRef = this.dialog.open(ConfirmDialogComponent, {data: {title: '删除项目', content: '确认删除项目？'}});
-        dialogRef.afterClosed().subscribe(result => console.log(result));
+        dialogRef.afterClosed().subscribe(result => {
+            console.log(result);
+            this.projects = this.projects.filter(p => p.id !== project.id);
+        });
     }
 
     launchInviteDialog() {
