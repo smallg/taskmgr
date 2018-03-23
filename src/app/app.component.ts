e68b48dee04e5e6a59094cfea5b498f5955ede09
gwpxjb@163.com <gwpxjb@163.com>
2018-03-23 15:43:30 +0800

    3-2 animation keyframs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {OverlayContainer} from '@angular/cdk/overlay';
-import {trigger, state, transition, style, animate} from '@angular/animations';
+import {trigger, state, transition, style, animate, keyframes} from '@angular/animations';
 
 @Component({
     selector: 'app-root',
@@ -8,12 +8,32 @@ import {trigger, state, transition, style, animate} from '@angular/animations';
     styleUrls: ['./app.component.scss'],
     animations: [
         trigger('square', [
-            state('green', style({'background-color': 'green', 'height': '200px', 'transform': 'translateX(0)'})),
-            state('red', style({'background-color': 'red', 'height': '100px', 'transform': 'translateX(100%)'})),
-            transition('green=>red', animate(1000)),
-            transition('red=>green', animate(1000))
+            state('green', style({'background-color': 'green', 'height': '100px', 'transform': 'translateY(-100%)'})),
+            state('red', style({'background-color': 'red', 'height': '100px', 'transform': 'translateY(100%)'})),
+            transition('green=>red', animate('.8s cubic-bezier(0,.8,.04,.32)')),
+            // transition('red=>green', animate('.8s cubic-bezier(0,.8,.04,.32)'))
+            transition('red=>green', animate(5000, keyframes([
+                style({transform: 'translateY(100%)'}),
+                style({transform: 'translateY(98%)'}),
+                style({transform: 'translateY(95%)'}),
+                style({transform: 'translateY(90%)'}),
+                style({transform: 'translateY(80%)'}),
+                style({transform: 'translateY(50%)'}),
+                style({transform: 'translateY(20%)'}),
+                style({transform: 'translateY(-10%)'}),
+                style({transform: 'translateY(-5%)'}),
+                style({transform: 'translateY(-2%)'}),
+                style({transform: 'translateY(0%)'}),
+                style({transform: 'translateY(20%)'}),
+                style({transform: 'translateY(-10%)'}),
+                style({transform: 'translateY(-40%)'}),
+                style({transform: 'translateY(-40%)'}),
+                style({transform: 'translateY(-40%)'}),
+                style({transform: 'translateY(100%)'})
+            ])))
         ])
     ]
+    // http://cubic-bezier.com/
 })
 export class AppComponent {
 
